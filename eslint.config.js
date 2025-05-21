edad00ceb1580aec638b050e1da542f924cd23b1
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-05-21 20:53:17 +0900

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
+      'react/display-name': 'off',
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: ['arrow-function', 'function-declaration'],
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.tsx'],
+        },
+      ],
+      'react/jsx-props-no-spreading': 'off',
+      'react/prop-types': 'off',
+      'react/require-default-props': 'off',
+    },
+  },
+  reactHooks.configs['recommended-latest'],
+]);
