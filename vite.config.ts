8a43db1602f6857970189109806364fd9bd7b724
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-10-04 17:29:20 +0900

    refactor: type definition

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,12 @@ import { defineConfig } from 'vite';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
-import dtsPlugin from 'vite-plugin-dts';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dtsPlugin({
-      include: ['lib'],
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
